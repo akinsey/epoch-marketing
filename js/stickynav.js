638307922476b5633653ee78c85ca9367bd769b4
Anthony Kinsey <anthony@slickage.com>
2015-07-13 10:39:35 -1000

    Added roadmap. Misc changes
    * Added roadmap page using Ed's google doc for roadmap
    
    * Updated title tags to have unique title per page
    
    * Updated nav menu active link css to include underline
    
    * Extracted javascript into separate js file for reuse

---
+++ js/stickynav.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var stickyNav = function() {
+    var pos = $(window).scrollTop(), breakpoint = 125, newNavPos;
+    if (pos >= breakpoint) { newNavPos = 5; }
+    else { newNavPos = breakpoint -1; }
+    $('.docs-nav').css('top', newNavPos);
+  };
+  $(window).on('scroll', stickyNav);
+  $(window).on('resize', stickyNav);
+});
